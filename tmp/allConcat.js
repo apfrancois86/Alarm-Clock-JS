cf14979acf45b7b1da8f0fc6f3ee4533347406f8
Alex Francois and Tracie Weitzman <alex@>
2017-01-03 13:28:30 -0800

    add setInterval

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,17 +1,20 @@
 var Alarm = require('./../js/alarm.js').alarmModule;
 
 $(document).ready(function(){
-  $('#time').text(moment().format('H:mm'));
+  setInterval(function() {$('#time').text(moment().format('H:mm:ss'));}, 1000);
   $('#alarmClock').submit(function(event){
     event.preventDefault();
     var setHour = parseInt($("#inputHour").val());
     var setMinute = parseInt($("#inputMinute").val());
     $('#showAlarm').text("Alarm is set for " + setHour + ":" + setMinute);
-    var currentHour = parseInt(moment().format('H'));
-    var currentMinute = parseInt(moment().format('mm'));
-    if ((setHour === currentHour) && (setMinute === currentMinute)) {
-      $('.alarmOn').show();
-      $('#showAlarm').hide();
+    function alarmChecker() {
+      var currentHour = parseInt(moment().format('H'));
+      var currentMinute = parseInt(moment().format('mm'));
+      if ((setHour === currentHour) && (setMinute === currentMinute)) {
+        $('.alarmOn').show();
+        $('#showAlarm').hide();
+      }
     }
+    setInterval(alarmChecker, 1000)
   });
 });
